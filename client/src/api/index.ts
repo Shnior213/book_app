9f9cf0da4e5b9413fc46d60b13096f32f6f320fd
Shnior Amir <shniramir@gmail.com>
2026-03-09 16:06:19 +0200

    added maping for books

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -36,6 +36,7 @@ export interface BookResponse {
 }
 
 export const api = axios.create({
+  
   baseURL: import.meta.env.VITE_API_URL,
 });
 
@@ -61,9 +62,12 @@ api.interceptors.response.use(
 
         if (!refreshToken) throw new Error("No refresh token");
 
-        const res = await axios.post(import.meta.env.VITE_API_URL + "/auth/refresh", {
-          refreshToken,
-        });
+        const res = await axios.post(
+          import.meta.env.VITE_API_URL + "/auth/refresh",
+          {
+            refreshToken,
+          },
+        );
 
         const { accessToken } = res.data;
 
@@ -112,7 +116,6 @@ export const logout = async (): Promise<void> => {
     localStorage.removeItem("accessToken");
     localStorage.removeItem("refreshToken");
     localStorage.removeItem("userId");
-    
   }
 };
 
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -3,11 +3,12 @@ import { getAllBooks, type BookResponse } from "../api";
 import { StyledNavLink } from "../styles/StyledNavLink";
 
 function HomePage() {
-  const [books, setBooks] = useState<BookResponse[]>();
+  const [books, setBooks] = useState<BookResponse[]>([]);
 
   useEffect(() => {
     const fetchBooks = async () => {
       const res = await getAllBooks();
+      console.log(res);
       setBooks(res);
     };
     fetchBooks();
@@ -18,13 +19,21 @@ function HomePage() {
       <StyledNavLink to={"/addbook"} style={{ width: "170px" }}>
         Add New Book
       </StyledNavLink>
-      {books?.map((book) => (
-        <div>
-          <div key={book.id}>{book.title}</div>
-          
-          <img src={book.image} alt={book.title}style={{ width: '200px', borderRadius: '8px'}}/>
-        </div>
-      ))}
+      {Array.isArray(books) ? (
+        books?.map((book) => (
+          <div key={book.id}>
+            <div>{book.title}</div>
+
+            <img
+              src={book.image}
+              alt={book.title}
+              style={{ width: "200px", borderRadius: "8px" }}
+            />
+          </div>
+        ))
+      ) : (
+        <p>Loading books or no books found</p>
+      )}
     </div>
   );
 }
